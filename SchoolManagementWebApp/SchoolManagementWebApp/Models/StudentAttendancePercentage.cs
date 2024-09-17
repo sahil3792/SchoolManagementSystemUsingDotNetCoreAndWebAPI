ec3a83acf7a6fc846f2b0aa238e3f134a8adcfaa
Windows <piyushmaurya7798@gmail.com>
2024-09-17 17:01:08 +0530

    Student Dashboard Changes

---
+++ SchoolManagementWebApp/SchoolManagementWebApp/Models/StudentAttendancePercentage.cs
@@ -0,0 +1,11 @@
+﻿namespace SchoolManagementWebApp.Models
+{
+    public class StudentAttendancePercentage
+    {
+        public Student student { get; set; }
+
+        public int PresentDays { get; set; }
+        public int TotalDaysInMonth { get; set; }
+        public double AttendancePercentage { get; set; }
+    }
+}
